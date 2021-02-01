af4bec67e21fc6727e781b9af811f13d82564b30
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-01 18:38:16 -0500

    TCP Testing
    -Tcp Testing

--- SocketConnectTest.ps1
+++ SocketConnectTest.ps1
@@ -1,5 +1,38 @@
 ﻿$port = 2020
-$endpoint = new-object System.Net.IPEndPoint ([IPAddress]::Any,$port)
-$udpclient = new-Object System.Net.Sockets.UdpClient $port
-$content = $udpclient.Receive([ref]$endpoint)
-[Text.Encoding]::ASCII.GetString($content)
+$ip = '127.0.0.1'
+$tcpClient = new-Object System.Net.Sockets.TcpClient ($ip,$port)
+$tcpStream = $tcpClient.GetStream()
+$reader = New-Object System.IO.StreamReader($tcpStream)
+$writer = New-Object System.IO.StreamWriter($tcpStream)
+$write.autoFlush = $true
+
+$buffer = New-Object System.Byte[] 1024
+$encoding = new-object System.Text.ASCIIEncoding
+
+while ($tcpClient.Connected)
+{
+    while ($tcpStream.DataAvailable)
+    {
+
+        $rawresponse = $reader.Read($buffer, 0, 1024)
+        $response = $encoding.GetString($buffer, 0, $rawresponse)   
+    }
+
+    if ($tcpClient.Connected)
+    {
+        Write-Host -NoNewline "prompt> "
+        $command = Read-Host
+
+        if ($command -eq "escape")
+        {
+            break
+        }
+
+        $writer.WriteLine($command) | Out-Null
+    }
+    start-sleep -Milliseconds 500
+}
+
+$reader.Close()
+$writer.Close()
+$tcpClient.Close()
