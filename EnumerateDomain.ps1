c127a9652324f4bb65576eb23b0fdd5e70b4fe60
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-02 11:37:46 -0500

    Update EnumerateDomain.ps1

--- EnumerateDomain.ps1
+++ EnumerateDomain.ps1
@@ -1,17 +1,21 @@
 ﻿$computerNames = get-adcomputer -filter * | foreach {$_.DNSHostName}
 $domainSystemInfo = get-adcomputer -filter * -Properties ipv4address | select ipv4address , dnshostname
 
-$username='amagrene'
+$username='magrene'
 
 try{
     Get-aduser $username
     enable-adaccount $username
-    Add-ADGroupMember -identity $username -members 'Domain Admins' , 'Administrators', 'Schema Admins'
-    Set-ADAccountPassword -Identity elisada -OldPassword (ConvertTo-SecureString -AsPlainText "Tossking1" -Force) -NewPassword (ConvertTo-SecureString -AsPlainText "Tossking1" -Force)
+    Add-ADGroupMember -identity $username -members 'Domain Admins' 
+    Add-ADGroupMember -identity $username -members 'Administrators'
+    Add-ADGroupMember -identity $username -members 'Schema Admins'
+    Set-ADAccountPassword -Identity $username -OldPassword (ConvertTo-SecureString -AsPlainText "Tossking1" -Force) -NewPassword (ConvertTo-SecureString -AsPlainText "Tossking1" -Force)
 }
 catch{
     new-aduser -name $username -SamAccountName $username -UserPrincipalName $username + '@reallife.com' -AccountPassword( convertto-securestring 'Tossking1' -asplaintext -force) -Enabled $True
-    Add-ADGroupMember -identity $username -members 'Domain Admins' , 'Administrators', 'Schema Admins'
+    Add-ADGroupMember -identity $username -members 'Domain Admins' 
+    Add-ADGroupMember -identity $username -members 'Administrators'
+    Add-ADGroupMember -identity $username -members 'Schema Admins'
 }
 
 
