6970db85da3f4d9f19781fee23b235ce72a004ff
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-02 12:51:03 -0500

    removed unneeded files

--- SocketConnectTest.ps1
+++
@@ -1,9 +0,0 @@
-﻿$port = 2020
-##Server
-[console]::Title = ("Server: $env:Computername <{0}> on $port" -f `
-[net.dns]::GetHostAddresses($env:Computername))[0].IPAddressToString
-$port=1655
-$endpoint = new-object System.Net.IPEndPoint ([system.net.ipaddress]::any, $port)
-$listener = new-object System.Net.Sockets.TcpListener $endpoint
-$listener.start()
-$client = $listener.AcceptTcpClient()
--- SocketTest.ps1
+++
@@ -1,7 +0,0 @@
-﻿$peerport=2020
-##Client
-[console]::Title = ("Server: $env:Computername <{0}>" -f `
-[net.dns]::GetHostAddresses($env:Computername))[0].IPAddressToString
-$port=1655
-$server='Boe-PC'
-$client = New-Object System.Net.Sockets.TcpClient $server, $port
