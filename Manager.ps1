3f35b6cd31afda6e5e37d41f5a3a7b729328a0d4
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-03 09:52:25 -0500

    Update Manager.ps1

--- Manager.ps1
+++ Manager.ps1
@@ -1,12 +1,19 @@
 ﻿$data = cat -Path C:\Users\AnthonyM\Documents\GitHub\PowershellShell\MachineDirectory.txt
 $credenital = Get-Credential
-Foreach($i in $data){
-    new-pssession $i -Credential $credenital
-    invoke-command -ComputerName $i -Credential $credenital -ScriptBlock { 
-        get-adcomputer -filter * -Properties ipv4address | select ipv4address , dnshostname
-    } | out-file ($i.substring(3,4) + ' TeamInfo')
 
+function getDomainStructure{
+    Foreach($i in $data){
+        new-pssession $i -Credential $credenital
+        $ip = Get-NetIPAddress | select ipaddress | Where-Object {$_.ipaddress -eq '10.*'}
+        if($ip -eq '*1.60*'){
+            invoke-command -ComputerName $i -Credential $credenital -ScriptBlock { 
+                get-adcomputer -filter * -Properties ipv4address | select ipv4address , dnshostname
+            } | out-file ($i.substring(3,4) + ' TeamInfo')
+        }
 
+    }
 }
+
+getDomainStructure
 
 #$domainSystemInfo = get-adcomputer -filter * -Properties ipv4address | select ipv4address , dnshostname
