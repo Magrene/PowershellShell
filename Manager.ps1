0a9ccf39c8035cb8703ed068bd71c644ab3d5de7
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-05 00:21:48 -0500

    Update Manager.ps1

--- Manager.ps1
+++ Manager.ps1
@@ -1,13 +1,35 @@
-﻿$data = cat -Path C:\Users\AnthonyM\Documents\GitHub\PowershellShell\MachineDirectory.txt
+﻿$data = cat -Path .\MachineDirectory.txt
 [string]$userName = 'virus\magrene'
 [string]$userPassword = 'Tossking123@'
-
 
 [SecureString]$secureString = $userPassword | ConvertTo-SecureString -AsPlainText -Force 
 [PSCredential]$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $secureString
 Foreach($i in $data){
+    $currentTeam = $i.substring(0,5) 
     Write-Output $i
-    Invoke-Command $i -Credential $credential -ScriptBlock {hostname}
+       
+        ($er = ($output=Invoke-Command $i -Credential $credential -ScriptBlock {hostname}) 2>&1) | Out-Null
+        Write-Output $output
+        if($er.Exception){
+            $targetIP=@()
+            Write-Output ('Failed to contact ' + $i)
+            Foreach($z in $data){
+                if($z.substring(0,5) -eq $i.substring(0,5) -and $i -ne $z) {
+                    if($targetIP -notcontains $z){
+                        $targetIP += $z          
+                        foreach($x in $targetIP){
+                            
+                            Write-Output ('Machine at ' + $x + ' executing command on machine ' + $i)
+                            invoke-command $x -Credential $credential -ArgumentList $i , $credential -ScriptBlock{invoke-command $args[0] -Credential $args[1] -ScriptBlock{hostname}} 
+                        }
+                    }
+
+                }
+                
+                
+            }
+       
+        }
 }
 
 function getDomainStructure{
